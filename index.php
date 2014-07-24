d7519db783f5e69cc4383ae38157aa632fc8eb5b
Lincoln Souza <emburaman+github@gmail.com>
2014-07-24 20:40:35 -0300

    First setup

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bootstrap, a sleek, intuitive, and powerful mobile first front-end framework for faster and easier web development.">
+    <meta name="keywords" content="HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+
+    <title>
+        Yashar
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
+    <link rel="icon" href="/favicon.ico">
+
+  </head>
+  <body>
+    <!-- Header Definitions -->
+    <div id="header">
+      <img class="logo" src="img/logo-orange-bg.png" alt="Yashar" />
+    </div>
+
+    <!-- Navigation Definitions -->
+    <div id="nav">
+      <ul class="menu">
+        <li><a href="" class="link">Home</a></li>
+        <li><a href="" class="link">Produtos</a></li>
+        <li><a href="" class="link">Contato</a></li>
+      </ul>
+    </div>
+  
+    <!-- Content Definitions -->
+    <div id="content">
+      
+    </div>
+    
+    <!-- Footer Definitions -->
+    <div id="footer">
+      
+    </div>
+  
+    <!-- Bootstrap core JavaScript ==================================================
+    Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script></script>
+  
+  </body>
+</html>
